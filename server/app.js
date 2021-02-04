afe10a4e9f9350979116b37081d0b0ef1de73141
Abril Veliz <amira.abril@gmail.com>
2021-02-04 12:33:17 -0300

    adding mern project

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Set up mongoose connection
-const mongoDB = process.env.MONGODB_URI || 'mongodb://root:products@localhost:27017/products';
+const mongoDB = process.env.MONGODB_URI || 'mongodb://silvestre:silvestre@localhost:27017/products';
 // const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/products';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
